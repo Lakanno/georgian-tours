fae40834b2f887d9bd266cb07cee9498eb34a715
Laka Nodia <112949971+Lakanno@users.noreply.github.com>
2026-05-11 17:23:23 +0400

    Add change

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/georgian-tours">
         <LanguageProvider>
           <Routes>
             <Route path="/" element={<Index />} />
